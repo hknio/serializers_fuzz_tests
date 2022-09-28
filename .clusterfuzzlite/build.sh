a21b699ea168c5aefe6cd0938cff6d754ebc4968
bbarwik <bbarwik@gmail.com>
2022-09-28 12:17:51 +0200

    Added seed corpus for libfuzzer

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,4 +4,9 @@
 cmake -DSERIALIZER=yas -B build -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC
 cmake --build build --parallel
 
-cp build/serialization_fuzz $OUT/
+cd build
+mkdir in
+./serialization_fuzz_generator
+zip -j serialization_fuzz_seed_corpus.zip in/*
+
+cp serialization_fuzz serialization_fuzz_seed_corpus.zip $OUT/
